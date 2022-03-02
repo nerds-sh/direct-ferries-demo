cf0f4f553685cb6a782f33a784e67e8a80f1245a
miutamihai <miuta.mihai@gmail.com>
2022-03-02 14:01:12 +0200

    [master] Add sass

--- src/dashboard/index.tsx
+++ src/dashboard/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import 'dashboard/style.sass'
 
-export const Dashboard = () => <div>
-  I am meant to represent a mighty dashboard
+export const Dashboard = () => <div className={'dashboard'}>
+  <div className={'dashboard-content'}>
+    <p>I am meant to represent a mighty dashboard</p>
+  </div>
 </div>
