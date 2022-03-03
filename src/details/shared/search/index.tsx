dd98bfa0f9f065f4aa585b03b3a177ef014e6319
Mihai Eugen Miuta <miuta.mihai@gmail.com>
2022-03-03 18:24:40 +0200

    [mihai] Setup formik (#12)
    * [mihai] Add person modals
    
    * [mihai] Setup form submission

--- src/details/shared/search/index.tsx
+++ src/details/shared/search/index.tsx
@@ -1,13 +1,19 @@
-import { Col, FormControl, InputGroup, Row } from 'react-bootstrap'
 import React from 'react'
+import { Col, FormControl, InputGroup, Row } from 'react-bootstrap'
+import { useContext } from 'details/context'
 
-export const Search = () => <Row className={'mt-4'}>
-  <Col>
-    <InputGroup>
-      <FormControl placeholder={'Enter a port, country or ferry company'}
-                   aria-label={'Search'}
-                   aria-describedby={'basic-addon2'} />
-      <InputGroup.Text id={'basic-addon2'}>Edit</InputGroup.Text>
-    </InputGroup>
-  </Col>
-</Row>
+export const Search = () => {
+  const { values, handleChange } = useContext()
+
+  return <Row className={'mt-4'}>
+    <Col>
+      <InputGroup>
+        <FormControl placeholder={'Enter a port, country or ferry company'}
+                     aria-label={'Search'}
+                     aria-describedby={'basic-addon2'}
+                     value={values.search} onChange={handleChange} name={'search'} />
+        <InputGroup.Text id={'basic-addon2'}>Edit</InputGroup.Text>
+      </InputGroup>
+    </Col>
+  </Row>
+}
