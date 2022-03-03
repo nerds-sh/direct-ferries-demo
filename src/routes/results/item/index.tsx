0632162f632bb1df24d2b53ac8116fa9f598a3cd
miutamihai <miuta.mihai@gmail.com>
2022-03-03 17:17:01 +0200

    [mihai] Navigate to details on item click

--- src/routes/results/item/index.tsx
+++ src/routes/results/item/index.tsx
@@ -3,8 +3,9 @@ import { Col, Container, Row } from 'react-bootstrap'
 import 'routes/results/item/style.scss'
 import { Origin } from 'routes/results/item/origin'
 import { Destination } from 'routes/results/item/destination'
+import { useOnClick } from 'routes/results/item/hooks'
 
-export const Item = () => <Container className={'itemContainer'}>
+export const Item = () => <Container className={'itemContainer'} onClick={useOnClick()}>
   <Row className={'mt-2 p-2'}>
     <Col>
       <Origin />
