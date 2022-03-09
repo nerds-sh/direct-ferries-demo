47033a6c238d9f69943957e6cedc9404d7ce1b65
Daniel Schiau <daniel.i.schiau@gmail.com>
2022-03-09 16:34:17 +0200

    Actual DF calls

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,31 @@ import 'details/shared/currency/style.scss'
 import { Form } from 'routes/form'
 import { Results } from 'routes/results'
 
+const params = {
+  'SearchTerm': '',
+  'AB_SAYT_ports': 'false',
+  'OtherPortSearchStr': '',
+  'legType': '1',
+  'cult': 'en-US',
+  'AB_user_iso_pop_route': 'False',
+  'userIso': 'RO',
+  'stdc': 'DF10COM',
+  'opname': '',
+  'country': '',
+  'port_from': '',
+  'port_to': '',
+  'baseData': '',
+  'callback': '',
+}
+
+const getDefaultRoutes = () => {
+  const query = new URLSearchParams(params).toString()
+  //this has to be a POST with query in body as it is
+  return `https://ssl.directferries.com/ferry/ws/dealpicker.asmx/SearchDataGetAB?${query}`
+}
+
 export const Routes = () => <>
   <Form />
   <Results />
+  <a href={getDefaultRoutes()}>Get default routes</a>
 </>
