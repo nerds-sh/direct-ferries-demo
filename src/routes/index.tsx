c8465b048a87943bfbfde7991769fa1c8da70188
Mihai Eugen Miuta <miuta.mihai@gmail.com>
2022-03-10 08:38:37 +0200

    [mihai] Get default routes on page render (#18)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,35 +2,12 @@ import React from 'react'
 import 'details/shared/currency/style.scss'
 import { Form } from 'routes/form'
 import { Results } from 'routes/results'
-
-const params = {
-  SearchTerm: '', // initial call without any searchterm but making another calls when typing the search term
-  AB_SAYT_ports: 'false',
-  OtherPortSearchStr: '',
-  legType: '1',
-  cult: 'en-US',
-  AB_user_iso_pop_route: 'False',
-  userIso: 'RO',
-  stdc: 'DF10COM',
-  opname: '',
-  country: '',
-  port_from: '',
-  port_to: '',
-  baseData: '',
-  callback: '',
-}
-
-const getDefaultRoutes = () => {
-  const query = new URLSearchParams(params).toString()
-
-  // this has to be a POST with query in body as it is
-  return `https://ssl.directferries.com/ferry/ws/dealpicker.asmx/SearchDataGetAB?${query}`
-}
+import { Behaviour } from 'routes/behaviour'
 
 export const Routes = () => <>
+  <Behaviour />
   <Form />
   <Results />
-  <a href={getDefaultRoutes()}>Get default routes</a>
 </>
 
 export * from 'routes/reducer'
