0d7907d544b03b5c5b85e44c543ebb4dd64cb0dc
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2022-03-11 19:44:05 +0200

    [chaos] Replace no-unused-vars rule (#32)

--- src/details/types/modal.ts
+++ src/details/types/modal.ts
@@ -1,12 +1,6 @@
-// Eslint is retarded again...
-
 export enum Modal {
-    // eslint-disable-next-line no-unused-vars
     None,
-    // eslint-disable-next-line no-unused-vars
     Adults,
-    // eslint-disable-next-line no-unused-vars
     Children,
-    // eslint-disable-next-line no-unused-vars
     Currency
 }
