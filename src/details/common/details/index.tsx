29ed09633871b8460e6b34351441792a6896af52
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2022-03-16 04:19:49 +0200

    [chaos] Remove useless folders (#59)

--- src/details/common/details/index.tsx
+++ src/details/common/details/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Pets } from 'src/details/common/pets'
 import { Persons } from 'src/details/common/persons'
 import { Title } from 'src/details/common/details/title'
-import { Transportation } from 'src/details/common/transportation'
+import { Transport } from 'src/details/common/transport'
 
 export const Details = () => <div className={'mt-4 mx-3'}>
   <Title />
   <Persons />
   <Pets className={'mt-2'} />
-  <Transportation className={'mt-3 mb-2'} />
+  <Transport className={'mt-3 mb-2'} />
 </div>
