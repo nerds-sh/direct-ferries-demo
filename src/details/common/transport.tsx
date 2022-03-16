97d485d8f40e185716352c67ba4ecd2170307fa2
radu <radu.buciuceanu@gmail.com>
2022-03-16 04:19:14 +0200

    [chaos] Remove useless folders

--- src/details/common/transport.tsx
+++ src/details/common/transport.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CLASS_NAMES = 'd-flex justify-content-between rounded p-2 bg-white '
 
-export const Transportation = ({ className }: any) => <div className={CLASS_NAMES + className}>
+export const Transport = ({ className }: any) => <div className={CLASS_NAMES + className}>
   <div className={'text-center w-100'}>Icon</div>
   <div className={'text-center w-100'}>Foot</div>
   <div className={'text-center w-100'}>Edit</div>
