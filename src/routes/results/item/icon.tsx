7091543c4b973c9593e7485e45b661008e4f0f10
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2022-03-16 03:00:58 +0200

    [chaos] Adjust routes alignments (#56)

--- src/routes/results/item/icon.tsx
+++ src/routes/results/item/icon.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export const Icon = () => <div>
+export const Icon = () => <div className={'w-100 text-center'}>
   ...
 </div>
--- src/routes/results/item/origin.tsx
+++ src/routes/results/item/origin.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes } from 'src/routes/types'
 
-export const Origin = ({ item }: {item: Routes.Item}) => <div className={'d-flex flex-column'}>
+export const Origin = ({ item }: {item: Routes.Item}) => <div className={'d-flex flex-column w-100'}>
   <div>{item.portFrom}</div>
   <div>{item.locationsFrom}</div>
 </div>
