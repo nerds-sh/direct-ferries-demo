538340858210c84e2b6523d942241592eb1a3cbd
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2022-03-16 04:21:30 +0200

    [chaos] Rename return into two-way (#60)

--- src/details/form.tsx
+++ src/details/form.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { OneWay } from 'src/details/one-way'
+import { TwoWay } from 'src/details/two-way'
 import { Type } from 'src/details/common/type'
 import { useContext } from 'src/details/context'
 import { Form as Forms } from 'src/details/types'
 import { Submit } from 'src/details/common/submit'
-import { ReturnTrip } from 'src/details/return-trip'
 
 export const Form = () => {
   const { type, handleSubmit } = useContext()
 
   return <form onSubmit={handleSubmit}>
     <Type />
-    {type === Forms.Return ? <ReturnTrip /> : <OneWay />}
+    {type === Forms.Return ? <TwoWay /> : <OneWay />}
     <Submit />
   </form>
 }
--- src/details/two-way/index.tsx
+++ src/details/two-way/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Search } from 'src/details/common/search'
 import { Details } from 'src/details/common/details'
-import { Dates } from 'src/details/return-trip/dates'
+import { Dates } from 'src/details/two-way/dates'
 import { Currency } from 'src/details/common/currency'
-import { DifferentReturn } from 'src/details/return-trip/different-return'
+import { DifferentReturn } from 'src/details/two-way/different-return'
 
-export const ReturnTrip = () => <>
+export const TwoWay = () => <>
   <Search />
   <Dates />
   <Details />
